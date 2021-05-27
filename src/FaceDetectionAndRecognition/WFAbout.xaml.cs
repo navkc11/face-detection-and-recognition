0df61b784377e6f43e781b4e37cf43a14981adb9
navkc11 <63931144+navkc11@users.noreply.github.com>
2021-05-27 06:35:57 +0300

    final update

--- src/FaceDetectionAndRecognition/WFAbout.xaml.cs
+++ src/FaceDetectionAndRecognition/WFAbout.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System.Windows;
+using FireSharp.Interfaces;
+using FireSharp.Config;
+using FireSharp.Response;
+using System;
 
 namespace FaceDetectionAndRecognition
 {
@@ -10,6 +14,46 @@ namespace FaceDetectionAndRecognition
         public WFAbout()
         {
             InitializeComponent();
+            Loaded += WFAbout_Loaded;
+        }
+
+        private void WFAbout_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(fcon);
+            }
+            catch
+            {
+                MessageBox.Show("There's a problem");
+            }
+        }
+
+        IFirebaseConfig fcon = new FirebaseConfig()
+        {
+            AuthSecret = "yfAOROgj22FDDAsmRD8b5BeFWUBr95oiOkPfTsut",
+            BasePath = "https://face-detection-1fc7d-default-rtdb.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            FaceData user = new FaceData()
+            {
+                TZ = txtTZ.Text,
+                PersonName = txtNewName.Text,
+            };
+
+            var setter = client.Update("User/" + txtTZ.Text.ToString(), user);
+            MessageBox.Show("Data Updated Succefully");
+            Close();
         }
     }
 }
